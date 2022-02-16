5e02ce9032a9897b05874c99799722af52815929
Rijul Sarji <64856177+rijulsarji@users.noreply.github.com>
2022-02-17 00:32:49 +0530

    resolved cors issue

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,17 @@ app.listen(port, () => {
   console.log(`Listening to port ${port}...`)
 })
 
+// for CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, x-auth-token'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
 // estabilishing connection with database
 const dbURI = process.env.DATABASE;
 mongoose
